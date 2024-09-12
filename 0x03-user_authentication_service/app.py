01b5000f3d528e0c292e9aba871a0aa4aa34031e
thattechgirlie <wanji.njoro1@gmail.com>
2024-09-12 16:56:16 +0300

    tasks

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from auth import Auth
+from flask import Flask, jsonify, request, abort, redirect
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def welcome() -> str:
+    """ define a welcome string"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """define user using POST"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ define login using POST """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if valid_login:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ define logout session using DELETE """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
