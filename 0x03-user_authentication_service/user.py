83341ba3cea3637d916fdc946f7e4fe5ebf0fc6a
thattechgirlie <wanji.njoro1@gmail.com>
2024-09-12 19:24:40 +0300

    corrections

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""task0 user.py"""
+
+from sqlalchemy import Column, Integer, String  # Import necessary types
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """defining class User"""
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """defining string representation"""
+        return f"User: id={self.id}"
