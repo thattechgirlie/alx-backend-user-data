0d9d68c07b7820ed395b6674ecc814c6e95224d5
thattechgirlie <wanji.njoro1@gmail.com>
2024-10-27 11:54:14 +0300

    corrections

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -60,7 +60,7 @@ def before_request():
         if not auth.current_user(request):
             abort(403)
 
-            
+  
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
